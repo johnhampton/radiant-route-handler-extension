dbbc55259f96257fa506fa3271195f313a677e22
John Hampton <john@cleanoffer.com>
2009-07-24 11:11:09 -0700

    Add <r:if_route_param /> and <r:unless_route_param /> tags.
    Added conditional tags to test whether a route parameter is set. The
    contents of the tags will be rendered if the condition is true.

--- app/models/route_handler_tags.rb
+++ app/models/route_handler_tags.rb
@@ -14,4 +14,31 @@ module RouteHandlerTags
     params && params[tag.attr['name'].to_sym]
   end
   
+  desc %{
+   Tests that a parameter from route handler is set. It has only one attribute - @name@, 
+   that contains name of parameter.
+   
+   *Usage:*
+   
+   <pre><code><r:route_param name="value_name" /></code></pre>
+  }
+  tag 'if_route_param' do |tag|
+    params = tag.locals.page.route_handler_params
+    
+    tag.expand if params && params[tag.attr['name'].to_sym]    
+  end
+  
+  desc %{
+   Tests that a parameter from route handler is not set. It has only one attribute - @name@, 
+   that contains name of parameter.
+   
+   *Usage:*
+   
+   <pre><code><r:route_param name="value_name" /></code></pre>
+  }
+  tag 'unless_route_param' do |tag|
+    params = tag.locals.page.route_handler_params
+    
+    tag.expand unless params && params[tag.attr['name'].to_sym]    
+  end
 end
--- spec/models/route_handler_tags_spec.rb
+++ spec/models/route_handler_tags_spec.rb
@@ -11,15 +11,42 @@ describe RouteHandlerTags do
     )
   end
   
-  it "should show Route Handler Parameter by <r:route_param /> tag" do
-    @page.route_handler_params = {:name => 'something'}
-    @page.should render("<r:route_param name='name' />").as('something')
+  describe "<r:route_param />" do
+    it "should show Route Handler Parameter by <r:route_param /> tag" do
+       @page.route_handler_params = {:name => 'something'}
+       @page.should render("<r:route_param name='name' />").as('something')
+     end
+
+
+     it "should substitute route handler params to any attribute of any tag" do
+       @page.route_handler_params = {:name => 'something', :something => "else"}
+       @page.should render("<r:route_param name='_:name:_' />").as('else')
+     end
+    
+  end
+   
+  describe "<r:if_route_param>" do
+    it "should render tag contents if the parameter exists" do
+      @page.route_handler_params = {:name => 'something'}
+      @page.should render("<r:if_route_param name='name'>Hello</r:if_route_param>").as('Hello')
+    end
+    
+    it "should not render tag contents if the parameter does not exist" do
+      @page.route_handler_params = {:name => 'something'}
+      @page.should render("<r:if_route_param name='notfound'>Hello</r:if_route_param>").as('')
+    end
+    
   end
   
-  
-  it "should substitute route handler params to any attribute of any tag" do
-    @page.route_handler_params = {:name => 'something', :something => "else"}
-    @page.should render("<r:route_param name='_:name:_' />").as('else')
+  describe "<r:unless_route_param>" do
+    it "should not render tag contents if the parameter exists" do
+      @page.route_handler_params = {:name => 'something'}
+      @page.should render("<r:unless_route_param name='name'>Hello</r:unless_route_param>").as('')
+    end
+    
+    it "should render tag contents if the parameter does not exists" do
+      @page.route_handler_params = {:name => 'something'}
+      @page.should render("<r:unless_route_param name='notfound'>Hello</r:unless_route_param>").as('Hello')
+    end
   end
-  
 end
